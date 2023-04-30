694e71011b4e4c374f45dcf404d36a7e61402cb4
Iceonq <bartek.sosnowicz@gmail.com>
2023-04-30 17:54:51 +0200

    .

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,14 +1,7 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-  contacts: [
-    { id: nanoid(), name: 'Alexander Weng', number: 997 },
-    { id: nanoid(), name: 'Mariush Weng', number: 991 },
-    { id: nanoid(), name: 'Ida Weng', number: 992 },
-    { id: nanoid(), name: 'Olga Weng', number: 993 },
-    { id: nanoid(), name: 'Hahmet Weng', number: 994 },
-    { id: nanoid(), name: 'Wariat Weng', number: 995 },
-  ],
+  contacts: [],
   filteredContacts: [],
 };
 
