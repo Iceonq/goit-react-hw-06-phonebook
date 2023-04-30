ef2d9f4300fe011e879cab90c6f55e597613a201
Iceonq <bartek.sosnowicz@gmail.com>
2023-04-30 17:51:59 +0200

    .

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,3 @@ import { contactsReducer } from './contactsSlice';
 export const store = configureStore({
   reducer: contactsReducer,
 });
-
-// import { devToolsEnhancer } from '@redux-devtools/extension';
-// import { createStore } from 'redux';
-// import { rootReducer } from './reducer';
-
-// const enhancer = devToolsEnhancer();
-// export const store = createStore(rootReducer, enhancer);
